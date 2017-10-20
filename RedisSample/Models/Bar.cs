b5dd5b661d089cafc8595d5959e753de8bb08932
Ivan Yurchenko <ivan.yurchenko.ua@gmail.com>
2017-10-20 18:32:35 +0300

    Added basic models

---
+++ RedisSample/Models/Bar.cs
@@ -0,0 +1,13 @@
+﻿namespace RedisSample.Models
+{
+    public class Bar
+    {
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+
+        public string Code { get; set; }
+
+        public virtual Foo Foo { get; set; }
+    }
+}
---
+++ RedisSample/Models/Foo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace RedisSample.Models
+{
+    public class Foo
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Title { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public virtual ICollection<Bar> Bars { get; set; }
+    }
+}
